134ca35a8c08feb42f0b47a0730e75462fdca35d
Muhammad Areeb <m.areebkhan125@gmail.com>
2021-08-04 06:47:16 -0800

    calculator

---
+++ app.js
@@ -0,0 +1,12 @@
+function getNumber(num){
+    var input=document.getElementById("input");
+    input.value+=num
+}
+function forClear(){
+    var input=document.getElementById("input");
+    input.value=""
+}
+function getResult(){
+    var input=document.getElementById("input");
+    input.value=eval(input.value)
+}
